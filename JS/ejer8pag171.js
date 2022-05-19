c1e509b4c0e741b7eaa6bfdf527002faf5ec175a
F0X18 <105197972+F0X18@users.noreply.github.com>
2022-05-19 13:51:37 +0200

    pendiente de revision

---
+++ JS/ejer8pag171.js
@@ -0,0 +1,19 @@
+//ejercio 8
+for (let i = 0; i < 51; i++) {console.log(i) };
+//ejercicio 9
+for (let i = 0; i < 51; i+=5 ) {console.log(i) };
+document.write()
+//ejercicio 10
+//let palabra = prompt("Escribe una palabra.");
+document.write(palabra,)
+//for (let palabra = 0; palabra < //array.length(11); index++) {
+  //  const element = array[palabra];
+    //document.write(palabra)}
+//ejercicio 11
+let numero=prompt
+for (let numero = 0; numero < array.length; numero--) 
+if () {''}{
+    const element = array[numero];
+    
+} numero = prompt("Escribe un numero.");
+document.write(numero)
