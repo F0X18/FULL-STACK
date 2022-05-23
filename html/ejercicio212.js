4a3f75dcc9de89da0a77e0fbbdaed40eb51fd695
F0X18 <105197972+F0X18@users.noreply.github.com>
2022-05-23 14:02:42 +0200

    123

---
+++ html/ejercicio212.js
@@ -0,0 +1,14 @@
+// ejercicio 1
+const [1,2,3,4,5,6]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+
+
+for (const iterator of object) {
+    
+}
