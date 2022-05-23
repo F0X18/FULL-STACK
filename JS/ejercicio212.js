87373dd16217a89cc1941719d6a4094c246d7bca
F0X18 <105197972+F0X18@users.noreply.github.com>
2022-05-23 22:00:56 +0200

    ejer 1

--- JS/ejercicio212.js
+++ JS/ejercicio212.js
@@ -1,6 +1,23 @@
 // ejercicio 1
-let i = "0"
-let text= ""
-for (let i = 0; i < 5; i++) {
-    text += "The number is " + i + "<br>";
-    }
+//for 
+function contador(num) {
+     for (let i = 0; i < 6; i++) {
+      console.log(i)
+         
+     }
+     
+ }
+ //array for
+const miArray=[0,1,2,3,4,5];
+miArray.forEach(function(item, index)   {
+    console.log("el valor de la posicion" +index+ " es: " +item)
+})
+
+//array for of
+let 
+for (const index of item) {
+    
+}
+
+
+ //ejercicio 2
