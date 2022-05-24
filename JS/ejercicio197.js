5514831d7923a77818705c6346cb52055968414e
F0X18 <105197972+F0X18@users.noreply.github.com>
2022-05-24 13:25:48 +0200

    objeto

---
+++ JS/ejercicio197.js
@@ -0,0 +1,30 @@
+/* ejercicio 1 : escribe un programa que liste el nombre de las propiedades del siguiente
+objeto utilizando las dos formas comentadas con anterioridad: { name : "David Rayy",
+sclass : "VI", rollno : 12 } */
+const David= { name : "David Rayy",
+'sclass' : "VI", rollno : 12 }
+console.log(David)
+let f = { name : "David Rayy",
+'sclass' : "VI", rollno : 12 }
+console.log(f);
+
+
+
+//ejercicio 2 escribe un programa que declare el siguiente objeto: { x: 1, y: 2 }. A continuación, elimina la propiedad x y añade una propiedad nueva z con valor 5.
+let x1 = (1)
+let y1= (2)
+delete x1
+console.log(x1)
+let z=(5)
+console.log(z)
+//ejercicio 3 escribe un programa que declare dos objetos: { x: 1, y: 2, z: 3 } y { a: 1, y: 2, z: 1 }. A continuación, comprueba qué nombres de propiedades son coincidentes y cuáles son también coincidentes en valor
+
+let x=1;y=2;z=3
+let a=1;y1=2;z1=1
+
+console.log(x == a);
+console.log(x===a)
+console.log(y == y1);
+console.log(y===y1)
+console.log(z == z1);
+console.log(z===z1)
--- JS/objeto.js
+++ JS/objeto.js
@@ -57,11 +57,121 @@ const persona = {
         nombre: 'Carmen',
         apellidos: 'González',
         datos: {
-        dirección: 'Sevilla'
+            dirección: 'Sevilla',
+            otrodato: 'freeze' ,
+            masdatos: {
+                info: 'moreno',
+        }
         }
         };
       // Sevilla
 console.log(persona1.datos.dirección);
 // undefined
 console.log(persona1.datos.otrodato);
+console.log(persona1.datos.masdatos.info);
 console.log(persona1.datos);
+
+const coche1 = { marca: 'Ford', modelo: 'Focus' };
+const coche2 = { marca: 'Ford', modelo: 'Focus' };
+// devuelve false porque no comparten referencia
+console.log(coche1 == coche2);
+// devuelve false porque no comparten referencia
+console.log(coche1 === coche2);
+// devuelve true porque el valor es el mismo y son de tipo string
+console.log(coche1.modelo === coche2.modelo);
+const coche3 = coche1;
+// devuelve true porque comparten referencia
+console.log(coche1 === coche3);
+//borrado de propiedades
+const x = {
+    peras: 3,fresas: 20
+};
+delete x.fresas;
+console.log(x)
+
+const x1 = {
+    peras: 3,
+    fresas: 20,
+    // no se debe utilizar aquí la sintaxis de la función de flecha porque this en ese caso apuntaría al objeto global
+    contar: function () {
+    console.log(this.peras + this.fresas);
+    }
+    };
+    // se guarda en un array el nombre de todas las propiedades del objeto x
+    const nombresPropiedades = Object.keys(x1);
+    // ["peras", "fresas", "contar"]
+    console.log(nombresPropiedades);
+    // true
+    console.log(nombresPropiedades.includes('fresas'));
+
+    const x2 = {
+        peras: 3,
+        fresas: 20,
+        contar: function() {
+        // no se puede utilizar aquí función de flecha porque this apuntaría al objeto global
+        console.log(this.peras + this.fresas);
+        }
+        };
+        // true
+console.log('fresas' in x2);
+
+// obligatorio utilizar function para posteriomente crear objetos con new
+const Person = function(name) {
+    // atributo público porque está declarado con this
+    this.name = name || '';
+    // atributo privado porque está declarado con const
+    const lastname = 'González';
+    // atributo privado porque está declarado con const
+    const myName = this.name;
+    // método privado porque está declarado con const
+    const fullName = () => {
+        return myName + ' ' + lastname;
+        };
+        // método público porque está declarado con this
+        this.introduce = () => {
+        return 'Hola, mi nombre es ' + fullName();
+        };
+        };
+        const oscar = new Person('Óscar');
+        // Óscar
+        console.log(oscar.name);
+        // Hola, mi nombre es Óscar González
+        console.log(oscar.introduce());
+        // undefined porque el atributo es privado
+        console.log(oscar.lastname);
+        // error porque el método no privado y no se puede acceder a él (es undefined) e invocar a undefined genera un error
+        //console.log(oscar.fullName());
+        console.log(oscar)
+        const objeto10 = {
+            nombre: 'Alejandro',
+            // función clásica de JavaScript sin function (opción recomendada)
+            mostrarNombre() {
+            console.log(this.nombre);
+            }
+            }
+            // Alejandro
+            objeto10.mostrarNombre();
+            const objeto200 = {
+            nombre: 'Alejandro',
+            // función clásica de JavaScript con function
+            mostrarNombre: function() {
+            console.log(this.nombre);
+            }
+            }
+            
+            const objeto100 = {
+                nombre: 'Alejandro',
+                // función clásica de JavaScript sin function (opción recomendada)
+                mostrarNombre() {
+                console.log(this.nombre);
+                }
+                }
+                // Alejandro
+                objeto10.mostrarNombre();
+                const objeto20 = {
+                nombre: 'Alejandro',
+                // función clásica de JavaScript con function
+                mostrarNombre: function() {
+                console.log(this.nombre);
+                }
+                }   
